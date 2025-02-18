f92246a0c1e42f35b463c4c79b10c8f0cee05d23
Kristina <christina@softhenge.com>
2025-02-18 11:18:11 +0400

    database management through sqlmanager

--- treeview.cpp
+++ treeview.cpp
@@ -2,50 +2,15 @@
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlError>
-#include <QFile>
-#include <QDir>
 #include <QDate>
 
 TreeView::TreeView(QObject *parent)
     : QObject(parent)
     , m_standardModel(new QStandardItemModel(this))
     , m_rootNode(m_standardModel->invisibleRootItem())
-    , m_watcher(new QFileSystemWatcher(this))
+    , m_SQLmanager(new SQLmanager())
 {
-    QSqlDatabase contactsDB = QSqlDatabase::addDatabase("QMYSQL");
-    contactsDB.setHostName("localhost");
-    contactsDB.setPort(3306);
-    contactsDB.setUserName("root");
-    contactsDB.setPassword("softhenge306");
-    contactsDB.setDatabaseName("my_database");
-    if (!contactsDB.open()) {
-        qDebug() << "Error: " << contactsDB.lastError().text();
-        return;
-    }
-
-    QSqlQuery query;
-    if (!query.exec("CREATE DATABASE IF NOT EXISTS my_database")) {
-        qDebug() << "Failed to create database:" << query.lastError().text();
-    } else {
-        qDebug() << "Database created successfully!";
-    }
-
-    QString createTableQuery = R"(
-                CREATE TABLE IF NOT EXISTS contacts (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
-                    name VARCHAR(255) NOT NULL UNIQUE,
-                    phone VARCHAR(20) NOT NULL,
-                    birthdate DATE NOT NULL,
-                    email VARCHAR(255) UNIQUE
-                )
-            )";
-
-    if (!query.exec(createTableQuery)) {
-        qDebug() << "Failed to create table:" << query.lastError().text();
-    } else {
-        qDebug() << "Table 'contacts' created successfully!";
-    }
-    getDataFromFile();
+    getDataFromDB();
 }
 
 TreeView::~TreeView()
@@ -54,50 +19,39 @@ TreeView::~TreeView()
 Q_INVOKABLE void TreeView::addContact(const QString& name,const QString& phone,const QString& birthDate,const QString& email) {
     QStringList dateParts = birthDate.split('/');
     QDate birthdate(dateParts[2].toInt(),dateParts[1].toInt(),dateParts[0].toInt());
-    QSqlQuery query;
-    query.prepare("INSERT IGNORE INTO contacts (name, phone, birthdate, email) "
-                  "VALUES (:name, :phone, :birthdate, :email)");
-    query.bindValue(":name", name);
-    query.bindValue(":phone", phone);
-    query.bindValue(":birthdate", birthdate);
-    query.bindValue(":email", email);
-    if (!query.exec()) {
-        qDebug() << "Failed to insert contact:" << query.lastError().text();
-    } else {
-        qDebug() << "Contact added successfully!";
-        QStandardItem* item = new QStandardItem(name);
 
-        auto row = prepareRow(phone, birthdate.toString("dd-MM-yyyy"), email);
+    QStringList list = m_SQLmanager->addContact(name, phone, birthdate, email);
+    QStandardItem* item = new QStandardItem(list[0]);
+    auto row = prepareRow(list[1], list[2], list[3]);
+    m_rootNode->appendRow(item);
+    item->appendColumn(row);
+}
 
-        m_rootNode->appendRow(item);
+Q_INVOKABLE void TreeView::onItemDoubleClicked(int row, int column) {
+    QModelIndex index = m_standardModel->index(row,column);
+    onDoubleClick(index);
+};
+
+Q_INVOKABLE void TreeView::filterWithKey(const QString& key) {
+    m_rootNode->removeRows(0, m_rootNode->rowCount());
+    QVector<QStringList> filteredContacts = m_SQLmanager->filterWithKey(key);
+    for (QStringList contactList : filteredContacts) {
+        QStandardItem* item = new QStandardItem(contactList[0]);
+        auto row = prepareRow(contactList[1], contactList[2], contactList[3]);
+        m_standardModel->appendRow(item);
         item->appendColumn(row);
-        emit modelChanged();
     }
 }
 
-void TreeView::getDataFromFile()
+void TreeView::getDataFromDB()
 {
     m_rootNode->removeRows(0, m_rootNode->rowCount());
-
-    QSqlQuery query;
-    if (!query.exec("SELECT name, phone, birthdate, email "
-                    "FROM contacts")) {
-        qDebug() << "Failed to retrieve contacts:" << query.lastError().text();
-    } else {
-        while(query.next()) {
-            QString name = query.value("name").toString();
-            qDebug() << name;
-            QString phone = query.value("phone").toString();
-            QString birthdate = query.value("birthdate").toString();
-            QString email = query.value("email").toString();
-
-            QStandardItem* item = new QStandardItem(name);
-
-            auto row = prepareRow(phone, birthdate, email);
-
-            m_rootNode->appendRow(item);
-            item->appendColumn(row);
-        }
+    QVector<QStringList> contactsVec = m_SQLmanager->getData();
+    for (QStringList contactList : contactsVec) {
+        QStandardItem* item = new QStandardItem(contactList[0]);
+        auto row = prepareRow(contactList[1], contactList[2], contactList[3]);
+        m_standardModel->appendRow(item);
+        item->appendColumn(row);
     }
 }
 
@@ -111,13 +65,5 @@ void TreeView::onDoubleClick(const QModelIndex &index)
 {
     QString name = m_standardModel->data(index).toString();
     m_standardModel->removeRow(index.row());
-    QSqlQuery query;
-    query.prepare("DELETE FROM contacts WHERE name = :name");
-    query.bindValue(":name",name);
-    if (!query.exec()) {
-        qDebug() << "Failed to delete contact:" << query.lastError().text();
-    } else {
-        qDebug() << "Contact deleted successfully!";
-    }
-    emit modelChanged();
+    m_SQLmanager->removeRow(name);
 }
--- treeview.h
+++ treeview.h
@@ -1,5 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "sqlmanager.h"
 
 #include <QMainWindow>
 #include <QTreeView>
@@ -12,33 +13,25 @@
 class TreeView : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QAbstractItemModel* model READ getModel NOTIFY modelChanged)
+    Q_PROPERTY(QAbstractItemModel* model READ getModel)
 public:
     TreeView(QObject *parent = nullptr);
     ~TreeView();
 
-    void getDataFromFile();
-
-    Q_INVOKABLE QAbstractItemModel* getModel() const { return m_standardModel; }
     Q_INVOKABLE void addContact(const QString& name,const QString& phone,const QString& birthDate,const QString& email);
-    Q_INVOKABLE void onItemDoubleClicked(int row, int column) {
-        QModelIndex index = m_standardModel->index(row,column);
-        onDoubleClick(index);
-        emit modelChanged();
-    };
+    Q_INVOKABLE void onItemDoubleClicked(int row, int column);
+    Q_INVOKABLE void filterWithKey(const QString& key);
+    Q_INVOKABLE QAbstractItemModel* getModel() const { return m_standardModel; }
 private:
+    void getDataFromDB();
     QList<QStandardItem *> prepareRow(const QString &first, const QString &second, const QString &third) const;
-
     //slots
     void onDoubleClick(const QModelIndex &index);
 
-signals:
-    void modelChanged();
 private:
     QStandardItemModel *m_standardModel;
     QStandardItem *m_rootNode;
-    QFileSystemWatcher *m_watcher;
-    QString m_filePath;
+    SQLmanager *m_SQLmanager;
 };
 
 #endif // MAINWINDOW_H
